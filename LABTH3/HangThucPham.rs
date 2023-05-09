df9e98aa8fbe373d24d0729aff0322bcf7cb6c5d
applenhi123456 <125206178+applenhi123456@users.noreply.github.com>
2023-05-09 23:19:22 +0700

    Update HangThucPham.rs

--- LABTH3/HangThucPham.rs
+++ LABTH3/HangThucPham.rs
@@ -1,1 +1,46 @@
-HangThucPham
+
+import java.time.LocalDate;
+
+class HangThucPham extends HangHoa {
+    private LocalDate ngaySanXuat;
+    private LocalDate ngayHetHan;
+    private String nhaCungCap;
+
+    public HangThucPham(String maHang, String tenHang, int soLuongTon, double donGia, LocalDate ngaySanXuat,
+            LocalDate ngayHetHan, String nhaCungCap) {
+        super(maHang, tenHang, soLuongTon, donGia);
+        this.ngaySanXuat = ngaySanXuat;
+        this.ngayHetHan = ngayHetHan;
+        this.nhaCungCap = nhaCungCap;
+    }
+
+    public LocalDate getNgaySanXuat() {
+        return ngaySanXuat;
+    }
+
+    public LocalDate getNgayHetHan() {
+        return ngayHetHan;
+    }
+
+    public String getNhaCungCap() {
+        return nhaCungCap;
+    }
+
+    @Override
+    public double tinhTienVAT() {
+        return getDonGia() * 0.05;
+    }
+
+    @Override
+    public String danhGiaMucDoBan() {
+        if (getSoLuongTon() == 0) {
+            return "Hết hàng";
+        } else if (getNgayHetHan().isBefore(LocalDate.now())) {
+            return "Khó bán";
+        } else {
+            return "Bán được";
+        }
+    }
+}
+
+
