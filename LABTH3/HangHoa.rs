4ed249a7f203e5bac7f97dd0284aee24e05ab0da
applenhi123456 <125206178+applenhi123456@users.noreply.github.com>
2023-05-09 23:20:45 +0700

    Update HangHoa.rs

--- LABTH3/HangHoa.rs
+++ LABTH3/HangHoa.rs
@@ -1,1 +1,41 @@
-HangHoa
+abstract class HangHoa {
+    private String maHang;
+    private String tenHang;
+    private int soLuongTon;
+    private double donGia;
+
+    public HangHoa(String maHang, String tenHang, int soLuongTon, double donGia) {
+        this.maHang = maHang;
+        this.tenHang = tenHang;
+        this.soLuongTon = soLuongTon;
+        this.donGia = donGia;
+    }
+
+    public String getMaHang() {
+        return maHang;
+    }
+
+    public String getTenHang() {
+        return tenHang;
+    }
+
+    public int getSoLuongTon() {
+        return soLuongTon;
+    }
+
+    public double getDonGia() {
+        return donGia;
+    }
+
+    public void setSoLuongTon(int soLuongTon) {
+        this.soLuongTon = soLuongTon;
+    }
+
+    public void setDonGia(double donGia) {
+        this.donGia = donGia;
+    }
+
+    public abstract double tinhTienVAT();
+
+    public abstract String danhGiaMucDoBan();
+}
